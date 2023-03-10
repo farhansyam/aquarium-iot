864d206ae0245b446d523f95e4e58ff1eae92b10
Cyberdesu <suredsi21@gmail.com>
2023-03-10 08:39:50 +0700

    gk guna

--- ESP32/main/GetSuhu.ino
+++ ESP32/main/GetSuhu.ino
@@ -1,21 +1,10 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
-
-
-float suhuDS18B20;
-
 float GetSuhu(){
-    sensor.requestTemperatures();
     sensor.requestTemperatures();       // send the command to get temperatures
     tempC = sensor.getTempCByIndex(0);  // read temperature in °C
-    tempF = tempC * 9 / 5 + 32; // convert °C to °F
 
     Serial.print("Temperature: ");
     Serial.print(tempC);    // print the temperature in °C
     Serial.print("°C");
-    Serial.print("  ~  ");  // separator between °C and °F
-    Serial.print(tempF);    // print the temperature in °F
-    Serial.println("°F");
-
+    return tempC
     delay(500);
 }
--- ESP32/main/main.ino
+++ ESP32/main/main.ino
@@ -1,8 +1,46 @@
-void setup(){
+#include "Configuration.h"
+#include <FirebaseESP32.h>
+FirebaseData firebaseData;
+#include <WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+OneWire oneWire(SENSOR_SUHU);
+DallasTemperature sensor(&oneWire);
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial){delay(100);}
+
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.println("******************************************************");
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_SSID);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    Firebase.begin(DATABASE_URL, API_KEY);
+    sensor.begin();
 
 }
+void loop(){
+    long randNumber;
+    randNumber = random(300);
+    Firebase.setFloat(firebaseData, "/admin/aquarium-1/ph", randNumber);
+    Firebase.setFloat(firebaseData, "/admin/aquarium-1/temp", randNumber);
+    Firebase.setFloat(firebaseData, "/admin/aquarium-1/turbidity", randNumber);
+    delay(1000);
+    GetSuhu();
 
-void loop()
-{
-    
 }
