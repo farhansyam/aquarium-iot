cdccee02a296d13bf91db752c6d6f6983429c343
Cyberdesu <suredsi21@gmail.com>
2023-04-02 13:05:03 +0700

    update dulu gk sih:v

--- ESP32/main/GetTurbidity.ino
+++ ESP32/main/GetTurbidity.ino
@@ -1,34 +1,42 @@
-float getTurbidity (){
+int pinTurb = 34;
+float V;
+float kekeruhan;
+float VRata2;
+float VHasil;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("SENSOR KEKERUHAN AIR");
+  delay(2000);
+}
+
+void loop()
+{
   V = 0;
   for(int i=0; i<800; i++)
   {
-    V += ((float)analogRead(14)/1023)*5;
+    V += ((float)analogRead(pinTurb)/4095)*3.3;
   }
+
   VRata2 = V/800;
   VHasil = roundf(VRata2*10.0f)/10.0f;
-  VFinal = VHasil-7;
-  kekeruhan = -1120.4*sq(VFinal)+5742.3*VFinal-4353.8; //Air Jernih 13488 //kalo sqrt itu = akar, kalo sq itu = perpangkatan 2
-  akhir = kekeruhan-13488;
-  if(VFinal <2.5){
+
+  if(VHasil < 1.25)
+  {
     kekeruhan = 3000;
-    Serial.print("tegangan :");
-    Serial.print(VFinal);
-    Serial.print(" V");
-    Serial.print("\t kekeruhan :");
-    Serial.println(kekeruhan);
-    tft.setCursor(4,189);
-    tft.setTextColor(TFT_RED); tft.setTextSize(3);
-    tft.print("KERUH ="); tft.print(kekeruhan); tft.println(" NTU");
-    }
-  else{
-    Serial.print("tegangan :");
-    Serial.print(VFinal);
-    Serial.print(" V");
-    Serial.print("\t kekeruhan :");
-    Serial.println(akhir);
-    tft.setCursor(4,189);
-    tft.setTextColor(TFT_RED); tft.setTextSize(3);
-    tft.print("KERUH ="); tft.print(akhir); tft.println(" NTU");
-    }
-  Serial.print("V sebenarnya : "); Serial.println(VHasil);
+  }
+  else
+  {
+    kekeruhan = -1120.4*pow(VHasil, 2) + 5742.3*VHasil - 4353.8;
+  }
+
+  Serial.print("Tegangan: ");
+  Serial.print(VHasil, 1);
+  Serial.print(" V");
+
+  Serial.print("\tKekeruhan: ");
+  Serial.print(kekeruhan);
+  Serial.println(" NTU");
+  delay(10);
 }
