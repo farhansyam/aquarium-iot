c360d2c8121dfcc690f73e6df143367b34b8b173
Cyberdesu <suredsi21@gmail.com>
2023-06-10 06:33:14 +0700

    update

--- ESP32/main/main.ino
+++ ESP32/main/main.ino
@@ -18,15 +18,27 @@
 #include "loading.h"
 #include "connections.h"
 #include <EEPROM.h> 
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+//#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <Fuzzy.h>
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define PIN        26 
+
+#define NUMPIXELS 16 // Popular NeoPixel ring size
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 
 static const int servoPin = 12;
 unsigned long previousMillis = 0;
-const unsigned long interval = 60000; // interval waktu dalam milidetik (1 menit)
+const unsigned long interval = 10000; // interval waktu dalam milidetik (1 menit)
 const int   daylightOffset_sec = 3600;
 char daysOfTheWeek[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
 
-RTC_DS1307 rtc;
+RTC_DS3231 rtc;
 FirebaseData firebaseData;
 Servo servo1;
 FirebaseData fbdo;
@@ -41,26 +53,56 @@ TwoWire myWire(1);
 //int d = 0;       // Variable used for the sinewave test waveform
 boolean range_error = 0;
 #define TFT_GREY 0x2104 // Dark grey 16 bit colour
-WiFiManager wm;
+//WiFiManager wm;
+Fuzzy *fuzzy = new Fuzzy();
+
+
+
+
+//input perbandingan PH
+FuzzySet *InputPhRendah = new FuzzySet(0, 0, 0.4, 0.6);
+FuzzySet *InputPhTinggi = new FuzzySet(1.4, 1.6, 2, 2);
+
+//input perbandingan kekeruhan
+FuzzySet *InputKekeruhanJernih = new FuzzySet(0, 3, 3, 5);
+FuzzySet *InputKekeruhanKeruh = new FuzzySet(5, 7, 7, 9);
+FuzzySet *InputKekeruhanSangatKeruh = new FuzzySet(9, 12, 12, 15);
+
+//fuzzyOutput
+FuzzySet *ServoCepat = new FuzzySet(0, 500, 500, 1000);
+FuzzySet *ServoSedang = new FuzzySet(1000, 1500, 1500, 2000);
+FuzzySet *ServoLama = new FuzzySet(2000, 2500, 2500, 3000);
+
+
  
 //unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 //unsigned long debounceDelay = 1000; // the debounce time; increase if the output flickers
 
+
 void setup() {
+
     myWire.begin(25, 32);
     WiFi.mode(WIFI_STA);
-    wm.setConnectTimeout(5);
+    //wm.setConnectTimeout(5);
+
+    #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+    #endif
+    // END of Trinket-specific code.
+
+    pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
     pinMode(relay1,OUTPUT);
-    if (! rtc.begin(&myWire)) {
+    if (!rtc.begin(&myWire)) {
         Serial.println(F("Couldn't find RTC"));
         Serial.flush();
         while (1) delay(10);
     }
 
-    if (! rtc.isrunning()) {
+    if (rtc.lostPower()) {
         Serial.println(F("RTC is NOT running, let's set the time!"));
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
+    //rtc.adjust(DateTime(2023, 5, 25, 16, 0, 12));
     if (!EEPROM.begin(1000)) {
         Serial.println(F("Failed to initialise EEPROM"));
         Serial.println(F("Restarting..."));
@@ -72,34 +114,16 @@ void setup() {
     tft.setRotation(1);
     uint16_t calData[5] = { 261, 3521, 367, 3422, 7 };
     tft.setTouch(calData);
-    wm.setCleanConnect(true);
-
-    /*sprite.setColorDepth(8);
-    sprite.createSprite(LOADING_BAR_WIDTH, LOADING_BAR_HEIGHT + 20);*/
-    /*uint16_t calData[5] = { 350, 3800, 350, 3400, 7 };
-    tft.setTouch(calData); */
+    sprite.setColorDepth(8);
+    sprite.createSprite(LOADING_BAR_WIDTH, LOADING_BAR_HEIGHT + 20);
 
     Serial.begin(115200);
     servo1.attach(servoPin);
     while(!Serial){delay(100);}
     
-    /*// We start by connecting to a WiFi network
-    Serial.println();
-    Serial.println("******************************************************");
-    Serial.print("Connecting to ");
-    Serial.println(WIFI_SSID);
-
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    */
     Serial.println(F("IP address: "));
     Serial.println(WiFi.localIP());
     sensor.begin();
-
     //firebase config
     config.api_key = API_KEY;
     config.database_url = DATABASE_URL;
@@ -107,22 +131,27 @@ void setup() {
     auth.user.password = USER_PASSWORD;
     Firebase.begin(&config, &auth);
 
-    if (Firebase.ready()) {
-        Serial.println(F("Firebase connection successful"));
-    } else {
-        Serial.println(F("Firebase connection failed"));
-    }
     for (int i = 0; i <= 100; i += 5) { // Change the increment value and max value to adjust loading speed
         drawLoadingBar();
         delay(100); // Change the delay time to adjust loading speed
     }
     tft.setSwapBytes(true);
     tft.fillScreen(TFT_BLACK);
+    fuzzyServo();
+    for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+
+        pixels.setPixelColor(i, pixels.Color(0, 0, 150));
+
+        pixels.show();   // Send the updated pixel colors to the hardware.
+
+        delay(200); // Pause before next pass through loop
+    }
 
 }
 void loop(){
     //wm.process();
     Serial.println(WiFi.status());
+    //digitalWrite(relay1,HIGH);
     PHsensor();
     getTurbidity();
     Serial.println(analogRead(34));
@@ -132,9 +161,11 @@ void loop(){
     // format tanggal
     String tanggal = (now.day() < 10 ? "0" : "") + String(now.day(), DEC) + "-" + (now.month() < 10 ? "0" : "") + String(now.month(), DEC) + "-" + String(now.year(), DEC);
     String waktu = (now.hour() < 10 ? "0" : "") + String(now.hour(), DEC) + ":" + (now.minute() < 10 ? "0" : "") + String(now.minute(), DEC);
+    Serial.println(waktu);
 
 
     uint8_t angka_random = random(0, 100);
+
 
     if(SwitchOn){
         ringMeter(kekeruhan, 0, 100, 180, 100, 63, "kekeruhan", GREEN2RED); // Draw analogue meter
@@ -147,9 +178,14 @@ void loop(){
             previousMillis = currentMillis; // store the current time as the previous time for the next iteration
             tft.fillRect(wifilogo_x, wifilogo_y, wifilogo_width, wifilogo_height, TFT_BLACK);
             tft.pushImage(wifilogo_x, wifilogo_y, wifilogo_width, wifilogo_height, connections);
+            if (Firebase.ready()) {
+                Serial.println(F("Firebase connection successful"));
+            } else {
+                Serial.println(F("Firebase connection failed"));
+            }
         }
     }
-    if (WiFi.status() == 6) {
+    if (WiFi.status() == 6 || WiFi.status() == 255 ) {
         unsigned long currentMillis = millis(); // get the current time
         if (currentMillis - previousMillis >= 5000) { // check if enough time has passed
             previousMillis = currentMillis; // store the current time as the previous time for the next iteration
@@ -193,22 +229,18 @@ void loop(){
 
     unsigned long currentMillis = millis();
     // Jika sudah lewat waktu 1 menit sejak data terakhir dikirim
-    if (Firebase.ready() && (currentMillis - previousMillis >= interval)) {
+    if (Firebase.ready() /*&& (currentMillis - previousMillis >= interval)*/) {
         // Simpan nilai millis saat ini ke dalam previousMillis
         previousMillis = currentMillis;
         FirebaseJson jVal;
         FirebaseJsonData result1;
         FirebaseJsonData result2;
         FirebaseJsonData result3;
-
-
-        
         if (Firebase.RTDB.setFloat(&fbdo, F("/admin/aquarium-1/ph"), Po) && Firebase.RTDB.setFloat(&fbdo, F("/admin/aquarium-1/temp"), Temperature) && Firebase.RTDB.setFloat(&fbdo, F("/admin/aquarium-1/turbidity"), kekeruhan) && Firebase.RTDB.setString(&fbdo, F("/admin/aquarium-1/updated_at"), tanggal)) {
             Serial.println(F("Data berhasil dikirim ke Realtime Database."));
         } else {
             Serial.printf("Data gagal dikirim ke Realtime Database. Error: %s\n", fbdo.errorReason().c_str());
         }
-
 
         //baca data dari rtdb untuk pakan  
         Serial.printf("Get json ref... %s\n", Firebase.RTDB.getJSON(&fbdo, F("/admin/aquarium-1/pakan"), &jVal) ? jVal.raw() : fbdo.errorReason().c_str());
@@ -225,33 +257,63 @@ void loop(){
             if(EEPROM.readString(addressFeedtime0) != myRtdbFeedTime[0]){
                 EEPROM.writeString(addressFeedtime0, myRtdbFeedTime[0]);
                 EEPROM.commit();
-                //Serial.printf("data disimpan ke EEPROM addressFeedtime0 dengan nilai string: %s",myRtdbFeedTime[0]);
+                Serial.printf("data disimpan ke EEPROM addressFeedtime0 dengan nilai string: %s",myRtdbFeedTime[0]);
             }
             else if(EEPROM.readString(addressFeedtime1) != myRtdbFeedTime[1]){
                 EEPROM.writeString(addressFeedtime1, myRtdbFeedTime[1]);
                 EEPROM.commit();
-                //Serial.printf("data disimpan ke EEPROM addressFeedtime1 dengan nilai string: %s",myRtdbFeedTime[1]);
+                Serial.printf("data disimpan ke EEPROM addressFeedtime1 dengan nilai string: %s",myRtdbFeedTime[1]);
 
             }
             else if(EEPROM.readString(addressFeedtime2) != myRtdbFeedTime[2]){
                 EEPROM.writeString(addressFeedtime2, myRtdbFeedTime[2]);
                 EEPROM.commit();
-                //Serial.printf("data disimpan ke EEPROM addressFeedtime2 dengan nilai string: %s",myRtdbFeedTime[2]);
+                Serial.printf("data disimpan ke EEPROM addressFeedtime2 dengan nilai string: %s",myRtdbFeedTime[2]);
 
             }else{
                 Serial.println(F("data sudah sama dengan eeprom"));
             }
-            //Serial.printf("pakan no 1: %s \n pakan no 2: %s \n  pakan no 3: %s",EEPROM.readString(addressFeedtime0),EEPROM.readString(addressFeedtime1),EEPROM.readString(addressFeedtime2));
+            Serial.printf("pakan no 1: %s \npakan no 2: %s \npakan no 3: %s",EEPROM.readString(addressFeedtime0),EEPROM.readString(addressFeedtime1),EEPROM.readString(addressFeedtime2));
         }
         localVarFeedTime[0] = EEPROM.readString(addressFeedtime0);
         localVarFeedTime[1] = EEPROM.readString(addressFeedtime1);
         localVarFeedTime[2] = EEPROM.readString(addressFeedtime2);
-        //Serial.printf("waktu pakan no 1: %s \n waktu pakan no 2: %s \n  waktu pakan no 3: %s",localVarFeedTime[0],localVarFeedTime[1],localVarFeedTime[2]);
-        getServo(waktu,localVarFeedTime,numFeed,timedel);
+        Serial.printf("waktu pakan no 1: %s \nwaktu pakan no 2: %s \nwaktu pakan no 3: %s",localVarFeedTime[0],localVarFeedTime[1],localVarFeedTime[2]);
+        //getServo(waktu,localVarFeedTime,numFeed,timedel);
 
 
 
     }
+
+
+
+    bool isServoRotated[numFeed] = {false}; // Array untuk melacak status putaran servo
+
+    for (int i = 0; i < numFeed; i++) {
+        if (waktu == localVarFeedTime[i] && !isServoRotated[i]) {
+            nilaiKekeruhanSaatMakan[i] = kekeruhan;
+            nilaiPhSaatMakan[i] = Po;
+            fuzzy->setInput(1, perbedaanKekeruhanSaatIni[i]);
+            fuzzy->setInput(2, perbedaanPhSaatIni[i]);
+            fuzzy->fuzzify();
+            int output = fuzzy->defuzzify(1);
+            servo1.write(130); // Menggerakkan servo
+            Serial.println("saatnya makan");
+            
+            delay(3000);
+            servo1.write(170); // Kembali ke posisi semula
+            
+            isServoRotated[i] = true; // Mengubah status putaran servo menjadi true
+            printf("isServoRotated[%d] = true\n", i);
+            
+            // Jika Anda ingin servo hanya berputar sekali pada setiap jam makan,
+            // Anda dapat keluar dari loop setelah servo berputar dengan menggunakan pernyataan "break"
+            break;
+        }
+            printf("blom waktunya makan\n");
+            printf("isServoRotated[%d] = %s\n", i, isServoRotated[i] ? "true" : "false");
+    }
+
 
 
 }
